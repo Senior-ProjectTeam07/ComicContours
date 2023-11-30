10de8927b6cf5118b9f763bb380a57fc816cf435
Rhiannon <brbrhiannon@aol.com>
2023-11-30 11:42:37 -0800

    jawline

--- Facial Landmarking/Facial_Landmarking.py
+++ Facial Landmarking/Facial_Landmarking.py
@@ -4,10 +4,8 @@ import csv
 import os
 import numpy as np
 
-
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
 
 def process_image(image_path):
     img = cv2.imread(image_path)
@@ -20,9 +18,10 @@ def process_image(image_path):
         all_landmarks = np.array([[landmarks.part(n).x, landmarks.part(n).y] for n in range(68)])
 
         features = {
-            'eyes': all_landmarks[36:48],
+            'jawline': all_landmarks[0:17],
             'eyebrows': all_landmarks[17:27],
             'nose': all_landmarks[27:36],
+            'eyes': all_landmarks[36:48],
             'lips': all_landmarks[48:68]
         }
 
@@ -39,12 +38,15 @@ def process_image(image_path):
 
     return all_faces_features
 
-
 if not os.path.exists('processed_images'):
     os.makedirs('processed_images')
 
-with open('image_paths.txt', 'r') as file:
-    image_paths = file.read().splitlines()
+image_directory = 'original_images'
+image_paths = [os.path.join(image_directory, filename) for filename in os.listdir(image_directory) if filename.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+with open('image_paths.txt', 'w') as file:
+    for path in image_paths:
+        file.write(path + '\n')
 
 with open('facial_features.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
@@ -57,6 +59,6 @@ with open('facial_features.csv', 'w', newline='') as csvfile:
                 for index, (x, y) in enumerate(landmarks):
                     writer.writerow([path, feature_type, f'Landmark {index + 1}', x, y])
 
-print("Processing complete. ")
-print("Mapping/landmarks are saved into facial_features.csv. ")
-print("Processed images are saved into the processed_images dir." )
+print("Processing complete.")
+print("Mapping/landmarks are saved into facial_features.csv.")
+print("Processed images are saved into the processed_images directory.")
