a696dbb93ff0997bdaa1e1839e16eb2c39f1b87e
Rhiannon <brbrhiannon@aol.com>
2023-12-05 23:06:25 -0800

    Augmenting image, math to find std dev (scaling needs work)

---
+++ Facial Landmarking/Augmenting_Features.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import os
+
+
+def load_feature_landmarks(facial_features, image_id, feature_to_int, feature_name):
+    feature_landmarks = facial_features[
+        (facial_features[:, 0] == image_id) & (facial_features[:, 1] == feature_to_int[feature_name])]
+    return feature_landmarks[:, 3:5]
+
+
+def resize_nose(img, facial_features, image_id, feature_to_int, scale_factor):
+    nose_landmarks = load_feature_landmarks(facial_features, image_id, feature_to_int, 'nose')
+    return resize_and_overlay_feature(img, nose_landmarks, scale_factor, width_margin_factor=0.2,
+                                      height_margin_factor=0.1)
+
+
+def resize_eyes(img, facial_features, image_id, feature_to_int, scale_factor):
+    eye_landmarks = load_feature_landmarks(facial_features, image_id, feature_to_int, 'eyes')
+    # Increased padding for eyes to capture eyelashes and surrounding area
+    return resize_and_overlay_feature(img, eye_landmarks, scale_factor, width_margin_factor=0.25,
+                                      height_margin_factor=0.4)
+
+
+def resize_and_overlay_feature(img, feature_points, scale_factor, width_margin_factor, height_margin_factor):
+    x_min, y_min = np.min(feature_points, axis=0).astype(int)
+    x_max, y_max = np.max(feature_points, axis=0).astype(int)
+    width_margin = int((x_max - x_min) * width_margin_factor)
+    height_margin = int((y_max - y_min) * height_margin_factor)
+    x_min = max(x_min - width_margin, 0)
+    y_min = max(y_min - height_margin, 0)
+    x_max = min(x_max + width_margin, img.shape[1])
+    y_max = min(y_max + height_margin, img.shape[0])
+    feature_region = img[y_min:y_max, x_min:x_max]
+    feature_width = x_max - x_min
+    feature_height = y_max - y_min
+    scaled_width = int(feature_width * scale_factor)
+    scaled_height = int(feature_height * scale_factor)
+    scaled_feature = cv2.resize(feature_region, (scaled_width, scaled_height), interpolation=cv2.INTER_LINEAR)
+    overlay_start_x = x_min + (feature_width - scaled_width) // 2
+    overlay_start_y = y_min + (feature_height - scaled_height) // 2
+    img[overlay_start_y:overlay_start_y + scaled_height,
+    overlay_start_x:overlay_start_x + scaled_width] = scaled_feature
+    return img
+
+
+def main():
+    facial_features = np.load('facial_features.npy')
+    feature_to_int = {'jawline': 0, 'eyebrows': 1, 'nose': 2, 'eyes': 3, 'lips': 4}
+    image_directory = 'original_images'
+    augmented_directory = 'augmented_images'
+    nose_scale_factor = 1.25
+    eye_scale_factor = 0.80
+
+    if not os.path.exists(augmented_directory):
+        os.makedirs(augmented_directory)
+
+    image_paths = [os.path.join(image_directory, filename) for filename in os.listdir(image_directory) if
+                   filename.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+    for img_path in image_paths:
+        img = cv2.imread(img_path)
+        image_id = image_paths.index(img_path)
+        img = resize_nose(img, facial_features, image_id, feature_to_int, nose_scale_factor)
+        img = resize_eyes(img, facial_features, image_id, feature_to_int, eye_scale_factor)
+
+        augmented_img_name = f"augmented_{os.path.basename(img_path)}"
+        augmented_img_path = os.path.join(augmented_directory, augmented_img_name)
+        cv2.imwrite(augmented_img_path, img)
+
+
+if __name__ == "__main__":
+    main()
--- Facial Landmarking/Facial_Landmarking.py
+++ Facial Landmarking/Facial_Landmarking.py
@@ -1,64 +1,65 @@
 import dlib
 import cv2
-import csv
 import os
 import numpy as np
+import csv
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-def process_image(image_path):
+def process_image_to_array(image_path, image_path_to_int, feature_to_int):
     img = cv2.imread(image_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = detector(gray)
-    all_faces_features = []
-
+    all_features_array = []
     for face in faces:
         landmarks = predictor(gray, face)
-        all_landmarks = np.array([[landmarks.part(n).x, landmarks.part(n).y] for n in range(68)])
+        for feature_name, feature_index in [('jawline', range(17)), ('eyebrows', range(17, 27)), ('nose', range(27, 36)), ('eyes', range(36, 48)), ('lips', range(48, 68))]:
+            for n in feature_index:
+                x, y = landmarks.part(n).x, landmarks.part(n).y
+                encoded_feature = [image_path_to_int[image_path], feature_to_int[feature_name], n, x, y]
+                all_features_array.append(encoded_feature)
+    return np.array(all_features_array)
 
-        features = {
-            'jawline': all_landmarks[0:17],
-            'eyebrows': all_landmarks[17:27],
-            'nose': all_landmarks[27:36],
-            'eyes': all_landmarks[36:48],
-            'lips': all_landmarks[48:68]
-        }
-
-        for feature in features.values():
-            for x, y in feature:
-                cv2.circle(img, (x, y), 3, (0, 255, 0), -1)
-
-        all_faces_features.append(features)
-
+def draw_landmarks_and_save(image_path, landmarks, output_directory):
+    img = cv2.imread(image_path)
+    for landmark in landmarks:
+        x, y = landmark[3], landmark[4]
+        cv2.circle(img, (x, y), 3, (0, 255, 0), -1)
     base_filename = os.path.basename(image_path)
-    output_filename = "processed_" + base_filename
-    output_path = os.path.join('processed_images', output_filename)
+    processed_filename = f"Processed_{base_filename}"
+    output_path = os.path.join(output_directory, processed_filename)
     cv2.imwrite(output_path, img)
 
-    return all_faces_features
+def save_array_to_csv(array, file_name):
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Image ID', 'Feature', 'Landmark Index', 'X', 'Y'])
+        writer.writerows(array)
 
 if not os.path.exists('processed_images'):
     os.makedirs('processed_images')
 
 image_directory = 'original_images'
 image_paths = [os.path.join(image_directory, filename) for filename in os.listdir(image_directory) if filename.lower().endswith(('.png', '.jpg', '.jpeg'))]
+image_path_to_int = {path: idx for idx, path in enumerate(image_paths)}
+feature_to_int = {'jawline': 0, 'eyebrows': 1, 'nose': 2, 'eyes': 3, 'lips': 4}
+all_features_list = []
 
-with open('image_paths.txt', 'w') as file:
-    for path in image_paths:
-        file.write(path + '\n')
+for path in image_paths:
+    features_array = process_image_to_array(path, image_path_to_int, feature_to_int)
+    all_features_list.append(features_array)
 
-with open('facial_features.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(['Image', 'Feature', 'Landmark', 'X Coordinate', 'Y Coordinate'])
+all_features = np.vstack(all_features_list)
+np.save('facial_features.npy', all_features)
 
-    for path in image_paths:
-        all_faces_features = process_image(path)
-        for features in all_faces_features:
-            for feature_type, landmarks in features.items():
-                for index, (x, y) in enumerate(landmarks):
-                    writer.writerow([path, feature_type, f'Landmark {index + 1}', x, y])
+save_array_to_csv(all_features, 'facial_features.csv')
 
-print("Processing complete.")
-print("Mapping/landmarks are saved into facial_features.csv.")
-print("Processed images are saved into the processed_images directory.")
+output_directory = 'processed_images'
+for path in image_paths:
+    current_image_landmarks = all_features[all_features[:, 0] == image_path_to_int[path]]
+    draw_landmarks_and_save(path, current_image_landmarks, output_directory)
+
+print("Success.")
+print("The landmarkings have been saved to a NumPy array, and facial_features.npy ")
+print("For double checking, the array data was also saved to facial_features.csv ")
